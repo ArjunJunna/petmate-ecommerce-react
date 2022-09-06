6dc16ad04723b89e3a11747d279ff3c41874ca51
Arjun Junna <arjun9852@gmail.com>
2022-09-06 20:35:49 +0530

    refactor

--- src/Routes/navbar-routes.jsx
+++ src/Routes/navbar-routes.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
-import ProductList from '../Pages/ProductList/ProductList';
 import LoginPage from '../Pages/Authentication/LoginPage';
+import ProductList from '../Pages/ProductList/ProductList';
+
 import MockmanAPI from '../Components/MockMan/mock-man';
 import SignupPage from '../Pages/Authentication/signuppage';
 import AuthorizedRoutes from '../Routes/authorized-routes';
@@ -14,7 +15,6 @@ function NavBarRoutes() {
       <Route index element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/product-list" element={<ProductList />} />
-
       <Route path="/login-page" element={<LoginPage />} />
       <Route path="/signup-page" element={<SignupPage />} />
       <Route path="/mockmanapi" element={<MockmanAPI />} />
@@ -22,6 +22,7 @@ function NavBarRoutes() {
         <Route path="/cart-list" element={<CartList />} />
         <Route path="/wish-list" element={<WishList />} />
       </Route>
+
       {/*<Route path="/cart-list" element={<CartList />} />
       <Route path="/wish-list" element={<WishList />} />*/}
     </Routes>
